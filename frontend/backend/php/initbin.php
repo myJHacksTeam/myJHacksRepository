0f2e76e376c33f61a4591d51f3982a2c2bb4069a
ericshermancs <zensap92@gmail.com>
2018-03-11 10:05:33 -0400

    fixed mismatch

--- frontend/backend/php/initbin.php
+++ frontend/backend/php/initbin.php
@@ -1,6 +1,7 @@
 <?php
 require("db_header.php");
-$sql = "INSERT INTO Trashcan (id, current_value, location, total_value) VALUES ('".$_POST["id"].",0,NULL,0')";
+$sql = "INSERT INTO Trashcan (id, current_value, location, total_value, nickname) VALUES ('".$_POST["id"].",0,NULL,0,Hillel')";
+echo $sql;
 $result = $conn->query($sql);
 if(!$result) {
 	echo $conn->error;
