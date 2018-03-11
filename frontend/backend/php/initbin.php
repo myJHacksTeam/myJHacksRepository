904dd677aa179b7e8e98c0ef4db8380193df5982
ericshermancs <zensap92@gmail.com>
2018-03-11 10:07:32 -0400

    bewp

--- frontend/backend/php/initbin.php
+++ frontend/backend/php/initbin.php
@@ -1,6 +1,6 @@
 <?php
 require("db_header.php");
-$sql = "INSERT INTO Trashcan (id, current_value, location, total_value, nickname) VALUES ('".$_POST["id"].",0,NULL,0,Hillel')";
+$sql = "INSERT INTO Trashcan (id, nickname, current_value, location, total_value) VALUES ('".$_POST["id"].",Hillel,0,NULL,0')";
 echo $sql;
 $result = $conn->query($sql);
 if(!$result) {
