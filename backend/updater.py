1daee5db50dded2481f7f4832739f32e8715a647
ericshermancs <zensap92@gmail.com>
2018-03-11 01:12:16 -0500

    rewrite updater

--- backend/updater.py
+++ backend/updater.py
@@ -1,7 +1,32 @@
+
+import requests
+
+
+
+
 def update_current_weight(value = 0, id = None):
-	pass
+	data = {
+	'value' : str(value),
+	'id' : str(id)
+	}
+	url = '10.0.2.255/update.php'
+	requests.post(url=url,data=data)
 
 
 def start_new_collection(value = 0, id = None):
-	pass
+	data = {
+	'value' : str(value),
+	'id' : str(id)
+	}
+	url = '10.0.2.255/newcollection.php'
+	requests.post(url=url,data=data)
+	
 
+
+def clear_all_stats(id = None):	# DEBUG ONLYs
+	data = {
+	'id' : str(id)
+	}
+	url = '10.0.2.255/clear.php'
+	requests.post(url=url,data=data)
+	
