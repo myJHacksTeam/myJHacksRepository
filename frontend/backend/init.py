6aecad3c587caf5459bf8d09ca9698134cf95bd6
ericshermancs <zensap92@gmail.com>
2018-03-11 15:31:35 -0400

    add argparse for debug

--- frontend/backend/init.py
+++ frontend/backend/init.py
@@ -1,5 +1,6 @@
 import updater
 import test
+import argparse
 location = input("Please enter the bin location: ")
 nickname = input("Please enter the bin nickname: ")
 
@@ -9,4 +10,8 @@ while not updater.init_bin(location = location, nickname = nickname):
 	location = input("Please enter the bin location: ")
 	nickname = input("Please enter the bin nickname: ")
 
-test.run()
+parser = argparse.ArgumentParser(description='init script for ReUniversity')
+parser.add_argument('-d','--debug',help='Flag to turn on manual captcha solving mode',default=False,action='store_true')
+args = parser.parse_args()
+
+test.run(args.debug)
