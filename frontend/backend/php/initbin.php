1d8aeda5ba714545b37c09834de34976c7db4915
ericshermancs <zensap92@gmail.com>
2018-03-11 10:00:30 -0400

    looking for traceback

--- frontend/backend/php/initbin.php
+++ frontend/backend/php/initbin.php
@@ -3,6 +3,7 @@ require("db_header.php");
 $sql = "INSERT INTO Trashcan (id, current_value, location, total_value) VALUES ('".$_POST["id"].",0,NULL,0')";
 $result = $conn->query($sql);
 if(!$result) {
+	echo $conn->error;
   die("Id not found");
 }
 echo "stuff";
