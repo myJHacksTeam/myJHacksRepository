85e29aef9417fd055c2bd919efb080a5d8703eaa
ericshermancs <zensap92@gmail.com>
2018-03-11 13:41:02 -0400

    FINAL COUNTDOWN

--- frontend/index.php
+++ frontend/index.php
@@ -48,7 +48,7 @@
                   }
                   echo "<tr class=\"leaderboard__item\"><td>#" . $i . "</td>";
                   echo "<td>" . $row["nickname"] . "</td>";
-                  echo "<td>" . $row["total_value"] . "</td>";
+                  echo "<td>" . $row["current_value"] . "</td>";
                   echo "<td><a class=\"map-link-row\" href=\"#\">Map</a></td></tr>";
                 }
               }
