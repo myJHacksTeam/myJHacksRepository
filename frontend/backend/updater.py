de8730446ff7bc5f855206ebb8a8d4b409e92f64
ericshermancs <zensap92@gmail.com>
2018-03-11 09:44:39 -0400

    fixed links

--- frontend/backend/updater.py
+++ frontend/backend/updater.py
@@ -9,7 +9,7 @@ def update_current_weight(value = 0, id_ = None):
 	'value' : str(value),
 	'id' : str(id_)
 	}
-	url = 'http://10.205.255.121/update.php'
+	url = 'http://10.205.255.121/frontend/backend/php/update.php'
 	return requests.post(url=url,data=data)
 
 
@@ -18,7 +18,7 @@ def start_new_collection(value = 0, id_ = None):
 	'value' : str(value),
 	'id' : str(id_)
 	}
-	url = 'http://10.205.255.121/newcollection.php'
+	url = 'http://10.205.255.121/frontend/backend/php/newcollection.php'
 	return requests.post(url=url,data=data)
 	
 
@@ -27,7 +27,7 @@ def clear_all_stats(id_ = None):	# DEBUG ONLYs
 	data = {
 	'id' : str(id_)
 	}
-	url = 'http://10.205.255.121/clear.php'
+	url = 'http://10.205.255.121/frontend/backend/php/clear.php'
 	return requests.post(url=url,data=data)
 	
 def init_bin(id_):
@@ -36,5 +36,5 @@ def init_bin(id_):
 	data = {
 	'id' : str(id_)
 	}
-	url = 'http://10.205.255.121/initbin.php'
+	url = 'http://10.205.255.121/frontend/backend/php/initbin.php'
 	return requests.post(url=url,data=data)
