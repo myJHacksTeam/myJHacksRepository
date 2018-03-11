868e30acc4d775bec26161179f199753f5e54c5f
ericshermancs <zensap92@gmail.com>
2018-03-11 09:56:33 -0400

    fixed db stuffs

--- frontend/backend/php/db_header.php
+++ frontend/backend/php/db_header.php
@@ -4,7 +4,7 @@ $username="user";
 $dbname="jhacks";
 $password="password";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($server, $username, $password, $dbname);
 if($conn->connect_error) {
   die("Failed to connect to the database.");
 } ?>
