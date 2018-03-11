d356eea09e01663e352fc0368b815bb172c361d3
ericshermancs <zensap92@gmail.com>
2018-03-11 10:18:33 -0400

    change me

--- frontend/backend/php/update.php
+++ frontend/backend/php/update.php
@@ -1,6 +1,7 @@
 <?php
 require("db_header.php");
-$sql = "UPDATE Trashcan SET current_value = " . $_POST["value"] . "WHERE id =". $_POST["id"];
+$sql = "UPDATE Trashcan SET current_value = " . $_POST["value"] . "WHERE id = ". $_POST["id"];
+echo $sql;
 $result=$conn->query($sql);
 if(!$result) {
 	echo $conn->error;
