66e6e4b1cb4e3470b81053ae6c59089338e7c98b
ericshermancs <zensap92@gmail.com>
2018-03-11 12:28:30 -0400

    change me

--- frontend/backend/php/initbin.php
+++ frontend/backend/php/initbin.php
@@ -1,7 +1,7 @@
 <?php
 require("db_header.php");
 $sql = "INSERT INTO Trashcan (nickname, current_value, location, total_value) VALUES (" . $_POST["nickname"]."',0,'".$_POST["location"]."',0)";
-echo $sql;
+//echo $sql;
 $result = $conn->query($sql);
 if(!$result) {
 	echo $conn->error;
