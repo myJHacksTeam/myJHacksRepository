5dbf571386150b2f18b5f5bf5241f52785b092e6
ericshermancs <zensap92@gmail.com>
2018-03-11 01:46:13 -0500

    did stuff

--- backend/updater.py
+++ backend/updater.py
@@ -4,29 +4,37 @@ import requests
 
 
 
-def update_current_weight(value = 0, id = None):
+def update_current_weight(value = 0, id_ = None):
 	data = {
 	'value' : str(value),
-	'id' : str(id)
+	'id' : str(id_)
 	}
 	url = '10.0.2.255/update.php'
 	requests.post(url=url,data=data)
 
 
-def start_new_collection(value = 0, id = None):
+def start_new_collection(value = 0, id_ = None):
 	data = {
 	'value' : str(value),
-	'id' : str(id)
+	'id' : str(id_)
 	}
 	url = '10.0.2.255/newcollection.php'
 	requests.post(url=url,data=data)
 	
 
 
-def clear_all_stats(id = None):	# DEBUG ONLYs
+def clear_all_stats(id_ = None):	# DEBUG ONLYs
 	data = {
-	'id' : str(id)
+	'id' : str(id_)
 	}
 	url = '10.0.2.255/clear.php'
 	requests.post(url=url,data=data)
 	
+def init_bin(id):
+	if not id_:
+		raise ValueError
+	data = {
+	'id' : str(id_)
+	}
+	url = '10.0.2.255/initbin.php'
+	requests.post(url=url,data=data)
