9e294f93e60221667845bb2c0ee1c5c8cf477749
Michael Bowe <michael_bowe@student.uml.edu>
2018-03-11 12:02:17 -0400

    fix addone.php

--- frontend/backend/php/addone.php
+++ frontend/backend/php/addone.php
@@ -7,9 +7,9 @@ if(!$result) {
 	echo $conn->error;
 	die("Id Not Found");
 }
-$curr_val = $result->fetch_object();
-$curr_val = $curr_val + 1;
-$sql = "UPDATE Trashcan SET current_value = " . $curr_val . " WHERE id = ". $_POST["id"];
+$curr_val = $result->fetch_[0]();
+$new_val = $curr_val[0] + 1;
+$sql = "UPDATE Trashcan SET current_value = " . $new_val . " WHERE id = ". $_POST["id"];
 echo $sql;
 $result=$conn->query($sql);
 if(!$result) {
